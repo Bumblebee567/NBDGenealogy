934d0bcede69911fbb24bf68ed8a2adb9a9e65ba
Bumblebee567 <maciekgasiorek9@gmail.com>
2018-03-30 13:38:40 +0200

    MVVM added

---
+++ NBDGenealogy/NBDGenealogy/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+using NBDGenealogy.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace NBDGenealogy
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<StartViewModel>();
+        }
+    }
+}
---
+++ NBDGenealogy/NBDGenealogy/Models/PersonModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBDGenealogy.Models
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+    public class PersonModel
+    {
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime DeathDate { get; set; }
+        public Gender Gender { get; set; }
+        public PersonModel Father { get; set; }
+        public PersonModel Mother { get; set; }
+        public List<PersonModel> Children { get; set; }
+
+    }
+}
