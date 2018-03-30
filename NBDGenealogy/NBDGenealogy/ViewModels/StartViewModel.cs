ad2e66dc0de6ee7b04ec7c895160d7144c6ad796
Bumblebee567 <maciekgasiorek9@gmail.com>
2018-03-30 22:22:39 +0200

    Modifications in StartView

--- NBDGenealogy/NBDGenealogy/ViewModels/StartViewModel.cs
+++ NBDGenealogy/NBDGenealogy/ViewModels/StartViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Caliburn.Micro;
 using NBDGenealogy.Models;
+using NBDGenealogy.Views;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,50 +9,12 @@ using System.Threading.Tasks;
 
 namespace NBDGenealogy.ViewModels
 {
-    public class StartViewModel : Conductor<object>
+    public class StartViewModel : Screen
     {
-        private string _name;
-        private DateTime _birthDate;
-        private DateTime _deathDate;
-        private Gender _gender;
-        private PersonModel _father;
-        private PersonModel _mother;
-        private List<PersonModel> _children;
-
-        public string Name
+        public void OpenAddPerson()
         {
-            get { return _name; }
-            set { _name = value; }
-        }
-        public DateTime BirthDate
-        {
-            get { return _birthDate; }
-            set { _birthDate = value; }
-        }
-        public DateTime DeathDate
-        {
-            get { return _deathDate; }
-            set { _deathDate = value; }
-        }
-        public Gender MyProperty
-        {
-            get { return _gender; }
-            set { _gender = value; }
-        }
-        public PersonModel Father
-        {
-            get { return _father; }
-            set { _father = value; }
-        }
-        public PersonModel Mother
-        {
-            get { return _mother; }
-            set { _mother = value; }
-        }
-        public List<PersonModel> Children
-        {
-            get { return _children; }
-            set { _children = value; }
+            var addPersonView = new AddPersonView();
+            addPersonView.Show();
         }
     }
 }
