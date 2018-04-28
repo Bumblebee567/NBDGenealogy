9a62f46013979ad1abb5f4adb2e263095752ede4
Bumblebee567 <maciekgasiorek9@gmail.com>
2018-04-28 21:16:03 +0200

    Adding person logic moved to AddPersonViewModel

--- NBDGenealogy/NBDGenealogy/ViewModels/AddPersonViewModel.cs
+++ NBDGenealogy/NBDGenealogy/ViewModels/AddPersonViewModel.cs
@@ -1,12 +1,146 @@
-﻿using System;
+﻿using Caliburn.Micro;
+using Db4objects.Db4o;
+using NBDGenealogy.Helpers;
+using NBDGenealogy.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NBDGenealogy.ViewModels
 {
-    class AddPersonViewModel
+    class AddPersonViewModel : Screen
     {
+        private string _name;
+        private DateTime _birthDate = DateTime.MinValue;
+        private DateTime _deathDate = DateTime.MinValue;
+        private EGender? _gender = null;
+        private PersonModel _father;
+        private PersonModel _mother;
+        private List<string> _children;
+        private ObservableCollection<PersonModel> _possibleFathers;
+        private ObservableCollection<PersonModel> _possibleMothers;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                NotifyOfPropertyChange(() => Name);
+            }
+        }
+        public DateTime BirthDate
+        {
+            get { return _birthDate; }
+            set
+            {
+                _birthDate = value;
+                NotifyOfPropertyChange(() => BirthDate);
+                AllPossibleFathers();
+                NotifyOfPropertyChange(() => PossibleFathers);
+                NotifyOfPropertyChange(() => PossibleMothers);
+            }
+        }
+        public DateTime DeathDate
+        {
+            get { return _deathDate; }
+            set
+            {
+                _deathDate = value;
+                NotifyOfPropertyChange(() => DeathDate);
+            }
+        }
+        public EGender? Gender
+        {
+            get { return _gender; }
+            set
+            {
+                _gender = value;
+                NotifyOfPropertyChange(() => Gender);
+            }
+        }
+        public PersonModel Father
+        {
+            get { return _father; }
+            set
+            {
+                _father = value;
+                NotifyOfPropertyChange(() => Father);
+            }
+        }
+        public PersonModel Mother
+        {
+            get { return _mother; }
+            set
+            {
+                _mother = value;
+                NotifyOfPropertyChange(() => Mother);
+            }
+        }
+        public List<string> Children
+        {
+            get { return _children; }
+            set { _children = value; }
+        }
+        public ObservableCollection<PersonModel> PossibleFathers
+        {
+            get { return AllPossibleFathers(); }
+            set
+            {
+                _possibleFathers = value;
+                NotifyOfPropertyChange(() => PossibleFathers);
+            }
+        }
+        public ObservableCollection<PersonModel> PossibleMothers
+        {
+            get { return AllPossibleMothers(); }
+            set
+            {
+                _possibleMothers = value;
+                NotifyOfPropertyChange(() => PossibleMothers);
+            }
+        }
+        public IList<EGender> GenderTypes
+        {
+            get
+            {
+                return Enum.GetValues(typeof(EGender)).Cast<EGender>().ToList<EGender>();
+            }
+        }
+
+        #region methods
+        public ObservableCollection<PersonModel> AllPossibleFathers()
+        {
+            IObjectContainer db = Db4oFactory.OpenFile("person.data");
+            ObservableCollection<PersonModel> possibleFathers = new ObservableCollection<PersonModel>();
+            var allMenInDatabase = db.QueryByExample(new PersonModel(EGender.Male));
+            foreach (var man in allMenInDatabase)
+            {
+                possibleFathers.Add((PersonModel)man);
+            }
+            db.Close();
+            possibleFathers = PossibleFathersHelper.RemovePossiblyWrongImportedFathers(possibleFathers);
+            if (BirthDate != DateTime.MinValue)
+                possibleFathers = PossibleFathersHelper.RemovePossiblyFathersWithWrongAge(possibleFathers, BirthDate);
+            return possibleFathers;
+        }
+        public ObservableCollection<PersonModel> AllPossibleMothers()
+        {
+            IObjectContainer db = Db4oFactory.OpenFile("person.data");
+            ObservableCollection<PersonModel> possibleMothers = new ObservableCollection<PersonModel>();
+            var allWomenInDatabase = db.QueryByExample(new PersonModel(EGender.Female));
+            foreach (var woman in allWomenInDatabase)
+            {
+                possibleMothers.Add((PersonModel)woman);
+            }
+            db.Close();
+            possibleMothers = PossibleMothersHelper.RemovePossiblyWrongImportedMothers(possibleMothers);
+            if (BirthDate != DateTime.MinValue)
+                possibleMothers = PossibleMothersHelper.RemovePossiblyMothersWithWrongAge(possibleMothers, BirthDate);
+            return possibleMothers;
+        }
+        #endregion
     }
 }
